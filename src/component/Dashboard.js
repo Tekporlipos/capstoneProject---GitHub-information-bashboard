0a17458c4ac65407a026d8df2778404836f6c6a8
nicocran1911 <nicholas.ocran@amalitech.org>
2022-09-02 13:51:21 +0000

    created repository component

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,28 +1,28 @@
-// import usericon from '../images/usericon.png'
-// import dashicon from "../images/dashicon.png";
-// import projecticon from "../images/projecticon.png";
-// import repositoryicon from "../images/repositoryicon.png";
+import usericon from '../images/usericon.png'
+import dashicon from "../images/dashicon.png";
+import projecticon from "../images/projecticon.png";
+import repositoryicon from "../images/repositoryicon.png";
 
-// const Dashboard = () => {
-//   return (
-//     <div className="dashcontainer">
-//       <div className="dash1">
-//       <img src={usericon} width={200} alt={usericon}/>
-//       <p>Username</p>
-//         <button className="editprofile"> Profile</button>
-//         <h1> <img src={dashicon} alt={dashicon} width={40}/>Dashboard</h1>
-//         <h1> <img src={projecticon} alt={projecticon} width={40}/> Projects</h1>
-//         <h1> <img src={repositoryicon} alt={repositoryicon} width={40} />Repositories</h1>
-//         <button className="signout"> Sign out</button>
-//       </div>
+const Dashboard = () => {
+  return (
+    <div className="dashcontainer">
+      <div className="dash1">
+      <img src={usericon} width={200} alt={usericon}/>
+      <p>Username</p>
+        <button className="editprofile"> Profile</button>
+        <h1> <img src={dashicon} alt={dashicon} width={40}/>Dashboard</h1>
+        <h1> <img src={projecticon} alt={projecticon} width={40}/> Projects</h1>
+        <h1> <img src={repositoryicon} alt={repositoryicon} width={40} />Repositories</h1>
+        <button className="signout"> Sign out</button>
+      </div>
 
-//       <div className="dash2">
-//         <h5> Recent Organisations</h5>
-//       </div>
-//     </div>
-//   );
-// }
+      <div className="dash2">
+        <h5> Recent Organisations</h5>
+      </div>
+    </div>
+  );
+}
 
 
-// export default Dashboard
+export default Dashboard
 
