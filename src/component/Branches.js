8f0d484d0d2508a58c3bf4d608992024e64c6021
nicocran1911 <nicholas.ocran@amalitech.org>
2022-09-06 21:37:00 +0000

    added contribution component

--- src/component/Branches.js
+++ src/component/Branches.js
@@ -1,8 +1,11 @@
 import usericon from "../images/usericon.png";
 import dashicon from "../images/dashicon.png";
+import pull from "../images/pull.png";
+import check from "../images/check.png";
 import projecticon from "../images/projecticon.png";
 import repositoryicon from "../images/repositoryicon.png";
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import Contributions from "./Contributions";
+import { DropdownButton, Dropdown, Card } from "react-bootstrap";
 
 const Dashboard = () => {
   return (
@@ -50,15 +53,40 @@ const Dashboard = () => {
           <DropdownButton
             variant="outline-secondary"
             id="dropdown-basic-button"
-            title="Main"
+            title="Collaborators"
           >
             <Dropdown.Item href="#/action-1">Footer</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            <Dropdown.Item href="#/action-1">Folder</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Features</Dropdown.Item>
           </DropdownButton>
+        </div>
+
+        <Card style={{ width: "15rem" }} className="pull">
+          <Card.Body>
+            <Card.Title class="text-center">
+              <img src={pull} alt={pull} width={25} />0
+            </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted" class="text-center">
+              Open pull request
+            </Card.Subtitle>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "15rem" }} className="pull1">
+          <Card.Body>
+            <Card.Title class="text-center">
+              <img src={check} alt={check} width={25} />
+              29
+            </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted" class="text-center">
+              Closed pull request
+            </Card.Subtitle>
+          </Card.Body>
+        </Card>
+
+        <div className="contributions">
+        Contributions 
+          <Contributions />
         </div>
       </div>
     </div>
